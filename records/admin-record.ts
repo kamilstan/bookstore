a9fa03a1226c6dd4d286c7e7a6924c7c71200e5d
Kamil Stangret <kamilstangret@wp.pl>
2022-08-25 12:32:07 +0200

    Add basic structure of AdminRecord with its types

---
+++ records/admin-record.ts
@@ -0,0 +1,33 @@
+import {AdminEntity} from "../types";
+import {ValidationError} from "../utils/handleErrors";
+
+export class AdminRecord implements AdminEntity {
+    id?: string;
+    user_id: string;
+    fullName: string;
+    email: string;
+
+    constructor(obj:AdminEntity) {
+        if (!obj.user_id) {
+            throw new ValidationError(' Pole "user_id" nie może być puste!');
+        }
+        if (!obj.fullName || obj.fullName.length > 100) {
+            throw new ValidationError('Pole "fullName" nie może być puste oraz przekracać 100 znaków!');
+        }
+        if (typeof obj.fullName !== 'string') {
+            throw new ValidationError('Format danych pola "fullName" jest nieprawidłowy!');
+        }
+        if (!obj.email || obj.email.length > 255) {
+            throw new ValidationError('Pole "e-mail" nie może być puste oraz nie może przekracać 255 znaków!');
+        }
+        if (typeof obj.email !== 'string') {
+            throw new ValidationError('Format danych pola "e-mail" jest nieprawidłowy!');
+        }
+
+        this.id = obj.id ?? null;
+        this.user_id = obj.user_id;
+        this.fullName = obj.fullName;
+        this.email = obj.email;
+    }
+
+}
