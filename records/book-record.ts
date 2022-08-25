e15553c4e5f14e076b664ce8f96d80400f54b3d7
Kamil Stangret <kamilstangret@wp.pl>
2022-08-25 11:35:11 +0200

    Add basic structure of BookRecord and its types

---
+++ records/book-record.ts
@@ -0,0 +1,64 @@
+import {BookEntity} from "../types";
+
+export class BookRecord implements BookEntity{
+    id?: string;
+    title: string;
+    author: string;
+    description: string;
+    price: number;
+    count: number;
+    review: number;
+
+    constructor(obj:BookEntity) {
+        if (!obj.title || obj.title.length > 200) {
+            throw new ValidationError('Pole "title" nie może być puste oraz nie może przekracać 200 znaków!');
+        }
+        if (typeof obj.title !== 'string') {
+            throw new ValidationError('Format danych pola "title" jest nieprawidłowy!');
+        }
+        if (!obj.author || obj.author.length > 100) {
+            throw new ValidationError('Pole "author" nie może być puste oraz nie może przekracać 100 znaków!');
+        }
+        if (typeof obj.author !== 'string') {
+            throw new ValidationError('Format danych pola "author" jest nieprawidłowy!');
+        }
+        if (!obj.description || obj.description.length > 1000) {
+            throw new ValidationError('Pole "description" nie może być puste oraz nie może przekracać 1000 znaków!');
+        }
+        if (typeof obj.description !== 'string') {
+            throw new ValidationError('Format danych pola "description" jest nieprawidłowy!');
+        }
+        if (!obj.price || obj.price > 999999) {
+            throw new ValidationError('Pole "price" nie może być puste oraz nie może być większe niż 999999!');
+        }
+        if (typeof obj.price !== 'number') {
+            throw new ValidationError('Format danych pola "price" jest nieprawidłowy!');
+        }
+        if (!obj.count || obj.count > 999999) {
+            throw new ValidationError('Pole "count" nie może być puste oraz nie może być większe niż 999999!');
+        }
+        if (typeof obj.count !== 'number') {
+            throw new ValidationError('Format danych pola "count" jest nieprawidłowy!');
+        }
+        if (!obj.price || obj.price > 999999) {
+            throw new ValidationError('Pole "price" nie może być puste oraz nie może być większe niż 999999!');
+        }
+        if (typeof obj.price !== 'number') {
+            throw new ValidationError('Format danych pola "price" jest nieprawidłowy!');
+        }
+        if (!obj.review || obj.review > 6) {
+            throw new ValidationError('Pole "review" nie może być puste oraz nie może być większe niż 6!');
+        }
+        if (typeof obj.review !== 'number') {
+            throw new ValidationError('Format danych pola "review" jest nieprawidłowy!');
+        }
+
+        this.id = obj.id ?? null;
+        this.title = obj.title;
+        this.author = obj.author;
+        this.description = obj.description;
+        this.price = obj.price;
+        this.count = obj.count;
+        this.review = obj.review;
+    }
+}
